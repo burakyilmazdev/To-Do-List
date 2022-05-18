249d83749cae416b10771f937868409633252528
Burak Yilmaz <burakyilmaz1627@gmail.com>
2022-05-18 19:42:58 +0300

    List screen has been designed.

---
+++ To Do List/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  To Do List
+//
+//  Created by Burak Yılmaz on 18.05.2022.
+//
+
+import Foundation
+
+struct Note{
+    var note:String
+    var isDone:Bool? = false
+    
+}
--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    var noteArray = [Note]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    
+}
 
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = noteArray[indexPath.row].note
+        return cell
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteArray.count
+    }
+    
+    
+    
 }
 
