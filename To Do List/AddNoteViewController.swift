5b9113f0bff47d7320ddf9989796481623dfa258
Burak Yilmaz <burakyilmaz1627@gmail.com>
2022-05-18 22:15:57 +0300

    Adding note screen and its segue have been done.

---
+++ To Do List/AddNoteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddNoteViewController.swift
+//  To Do List
+//
+//  Created by Burak Yılmaz on 18.05.2022.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
